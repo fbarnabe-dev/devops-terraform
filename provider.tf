8e0ad00dd367f443c575abb02ec480df7f4148a5
Felipe Barnabé <fbarnabe8@gmail.com>
2024-12-19 15:49:22 -0300

    feat: remote backend

--- provider.tf
+++ provider.tf
@@ -1,12 +1,17 @@
 terraform {
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "5.81.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.81.0"
     }
+  }
+  backend "s3" {
+    bucket = "devopsdemo-bc"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
