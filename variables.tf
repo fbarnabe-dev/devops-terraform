d70c1c593ff22e986eb303d06fb8a541e4b508a6
fbarnabe-dev <fbarnabe8@gmail.com>
2024-12-20 15:03:20 -0300

    feat: private subnets and variables

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "cidr_block" {
   type        = string
   description = "Networking CIDR block to be used fot he VPC"
 }
+
+variable "project_name" {
+  type        = string
+  description = "Project name to be used to name the resources (Name tag)"
+}
